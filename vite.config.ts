d05d18e93490d07ccff36d5018c9a6fe27509a87
Vinitha8008 <vinipeddireddy@gmail.com>
2025-08-03 18:29:44 +0530

    Packagefileschanged

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Vinitha_Portfolio_Website/", // 👈 ADD THIS LINE
   server: {
     host: "::",
     port: 8080,
